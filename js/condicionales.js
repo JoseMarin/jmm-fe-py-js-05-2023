97efb5a263154dedb792a4c590c73fbbcff9492e
JoseMarin <jmarinmalagon@gmail.com>
2023-05-23 19:02:15 +0200

    condicionales

---
+++ js/condicionales.js
@@ -0,0 +1,62 @@
+// let A = -2;
+// let B = 10;
+
+// if (A > 0) {
+//     if (A > 3) {
+//         console.log('A es mayor a 3');
+//     } else {
+//         console.log('A es menor a 3');
+//     }
+// } else {
+//     console.log('A es mmenor a 0');
+// }
+
+
+// if (A > B) {
+//     console.log('A es mayor');    
+// }
+
+// if (A < B) {
+//     console.log('B es menor');
+// }
+
+// if (A == B) {
+//     console.log('A es igual que B');
+// }
+
+// if (A > B) {
+//     console.log('A es mayor');
+// }else if(A < B){
+//     console.log('B es mayor');
+// } else {
+//     console.log('A y B son iguales');
+// }
+
+
+// set de datos acotados
+let dia = 6;
+
+switch (dia) {
+    case 1:
+        console.log('lunes');
+        break;
+    case 2:
+        console.log('martes');
+        break;
+    case 3:
+        console.log('miercoles');
+        break;
+    case 4:
+        console.log('jueves');
+        break;
+    case 5:
+        console.log('viernes');
+        break;
+    default:
+        console.log('No es un dia laborable');
+        break;
+}
+
+if (dia == 1) {
+    console.log('lunes');
+}
