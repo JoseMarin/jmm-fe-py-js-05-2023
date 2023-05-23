d19ec690aa58e1f954b9986385daf6f6a3fe50a3
JoseMarin <jmarinmalagon@gmail.com>
2023-05-23 16:32:21 +0200

    new project js

---
+++ js/index.js
@@ -0,0 +1,15 @@
+//Ejemplo de comentario
+
+/* ejemplo de comentario
+multilínea
+    */
+
+console.log('Hola Mundo');
+console.log("Hola Mundo");
+console.log('Hola Mundo');
+console.log('Hola Mundo');
+
+prompt('Introduce tu nombre');
+
+alert('Bienvenido a mi web!');
+
---
+++ js/variables.js
@@ -0,0 +1,12 @@
+// tipos de variables en js
+var nombre = 'Jose'; // mejor let 
+let edad = 34;
+let nota = -3.45;
+const esProfe = true; 
+const IVA = 0.21; 
+
+console.log(nombre);
+
+nombre = 1234;
+// esProfe = false;
+console.log(nombre);
